54c499b0e44a1d8e16da5401697e2b5f4d6adb7c
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-02-24 08:49:01 +0100

    test de de la clase articulo

--- practica2/src/test/java/ejercicio3/ArticuloTest.java
+++ practica2/src/test/java/ejercicio3/ArticuloTest.java
@@ -1,5 +1,21 @@
 package ejercicio3;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ArticuloTest {
 
+    @Test
+
+    void testPrecioFinal() {
+        Articulo articulo = new Articulo("RAM", 5.25, TipoIVA.GENERAL);
+
+        var resultado = articulo.getTipoIVA().calcularPrecioFinal(articulo.getPrecio());
+
+        assertEquals(6.5625, resultado, 0.01, "El precio final deberia ser aprox. 6.5625");
+    }
+
+    void testPrecioNegativo() {
+        Articulo articulo = new Articulo("NVIDIA GTX 5090", -55, TipoIVA.REDUCIDO);
+    }
 }
