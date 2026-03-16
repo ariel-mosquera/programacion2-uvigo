6bf984954c39ee9a27fd5e25850f75e1c53de81b
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-03-16 13:05:50 +0100

    Solución del ejercicio 1 de la practica 3

--- practica3/src/main/java/ejercicio1/Division.java
+++ practica3/src/main/java/ejercicio1/Division.java
@@ -9,20 +9,36 @@ public class Division {
     }
 
     public static void main(String[] args) {
-        int num1;
-        int num2;
+        int num1 = 0;
+        int num2 = 0;
         String linea;
         Scanner scan = new Scanner(System.in);
+        boolean valido = true;
 
-        System.out.print("\nIntroduzca dividendo: ");
-        linea = scan.nextLine();
-        num1 = Integer.parseInt(linea);
+        do {
+            try {
 
-        System.out.print("\nIntroduzca divisor: ");
-        linea = scan.nextLine();
-        num2 = Integer.parseInt(linea);
+                System.out.print("\nIntroduzca dividendo: ");
+                linea = scan.nextLine();
+                num1 = Integer.parseInt(linea);
 
-        System.out.println("\nEl resultado es: " + divide(num1, num2));
+                System.out.print("\nIntroduzca divisor: ");
+                linea = scan.nextLine();
+                num2 = Integer.parseInt(linea);
+
+                valido = true;
+
+            } catch (NumberFormatException e) {
+                System.out.println("Lo que introduciste no fue un entero, vuelve a intentar...");
+                valido = false;
+            }
+        } while (!valido);
+
+        try {
+            System.out.println("\nEl resultado es: " + divide(num1, num2));
+        } catch (ArithmeticException e) {
+            System.out.println("La división por " + num2 + " no está definida");
+        }
 
         scan.close();
     }
