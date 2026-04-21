fe322089eaeb8d1ec5b21e5110e1e343c53d4a1d
Ariel Mosquera Ribeiro <arielmosquera015@outlook.com>
2026-04-22 00:16:50 +0200

    Add interactive menu program for ejercicio2

---
+++ practica5/src/main/java/ejercicio2/Programa.java
@@ -0,0 +1,47 @@
+package ejercicio2;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Programa {
+
+    private Scanner scanner = new Scanner(System.in);
+    private ArrayList<Operacion> operaciones;
+
+    public Programa(ArrayList<Operacion> operaciones) {
+        this.operaciones = operaciones;
+    }
+
+    public void iniciar() {
+        int opcion = 0;
+        int tamanhoOperaciones = operaciones.size();
+        boolean valido = true;
+
+        while (opcion != tamanhoOperaciones || valido) {
+            System.out.println("\n=== Menú ===\n");
+
+            for (Operacion operacion : operaciones)
+                System.out.printf("Opción (%d) -- %s\n", (operaciones.indexOf(operacion) + 1), operacion.getDescripcion());
+
+            System.out.print("\nOpción: ");
+            opcion = leerEntero();
+
+            if (opcion >= 1 && opcion <= tamanhoOperaciones)
+                operaciones.get(opcion - 1).ejecutar();
+            else {
+                System.out.println("Opción inválida");
+                valido = false;
+            }
+        }
+    }
+
+    private int leerEntero() {
+        while (true) {
+            try {
+                return Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.print("Por favor, introduce un número: ");
+            }
+        }
+    }
+}
